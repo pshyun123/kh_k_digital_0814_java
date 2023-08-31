626c7a2032124b02713877a4003d88a444519b0b
psh7204 <pshyun7204@gmail.com>
2023-08-31 17:50:14 +0900

    20230831

--- src/단어정렬하기/WordSortEx.java
+++ src/단어정렬하기/WordSortEx.java
@@ -1,4 +1,36 @@
 package 단어정렬하기;
-//
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Scanner;
+
+//알파벳 소문자로 이루어진 N개의 단어가 들어오면
+//1. 길이가 짧은 것부터
+//2. 길이가 같으면 사전 순으로 정렬
 public class WordSortEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //n의 개수 입력받기
+        String[] word = new String[n]; //n개의 문자열 배열 생성
+        for (int i = 0; i < word.length; i++) {
+            word[i] = sc.next();
+        }
+        //입력받은 문자열 배열의 중복제거를 위해 set 컬렉션에 넣음
+        HashSet<String> set = new HashSet<>(Arrays.asList(word));
+        //다시 원래 배열로 되돌림, 원래 생성된 크기만큼 되돌림(넣었다 빼기)
+        word = set.toArray(new String[0]);
+        Arrays.sort(word, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) { //길이비교, 같은 것 부터 사전순 정렬
+                    return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length(); //만약 앞이 길면 양수
+                }
+            }
+        });
+        System.out.println(Arrays.toString(word)); //향상된 포문 말고 tostring 도 가능
+    }
 }
+
--- src/학생성적정렬/StudentSortEx.java
+++ src/학생성적정렬/StudentSortEx.java
@@ -1,5 +1,42 @@
 package 학생성적정렬;
 //5명의 학생에 대해 이름과 성적 입력. 성적 높은사람 수능로 정렬, 성적이 같으면 이름이 정렬
-//
+//미리 입력 받아두고 시작한다.
+//컴페어러블을 이름,성적이 포함된 클래스만들어서
+import 클론오브젝트.Member;
+
+import java.util.TreeSet;
+
+
 public class StudentSortEx {
+    public static void main(String[] args) {
+        TreeSet<StudentInfo> info = new TreeSet<>();
+        info.add(new StudentInfo("안유진", 95));
+        info.add(new StudentInfo("장원영", 77));
+        info.add(new StudentInfo("ㅇㅇㅇ", 65));
+        info.add(new StudentInfo("ㄷㄷㄷ", 79));
+        info.add(new StudentInfo("ㄹㄹㄹ", 80));
+        for(StudentInfo e : info) {
+            System.out.print(e.name + ", " + e.score);
+        }
+
+    }
 }
+
+    class StudentInfo implements Comparable<StudentInfo>{
+        String name;
+        int score;
+        public StudentInfo(String name, int score){
+            this.name = name;
+            this.score =score;
+        }
+
+        @Override
+        public int compareTo(학생성적정렬.StudentInfo o) {
+            if(this.score < o.score) return 1; //내림차순 정렬
+            else{
+                return this.name.compareTo(o.name); //사전순 정렬. 앞에값이 크면 뒤집는다.(return 1)
+            }
+        }
+    }
+
+
